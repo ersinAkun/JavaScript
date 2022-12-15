a50a517084111719ce173819ba0ee666557fcb47
Ersin Akun <ersinakun34@gmail.com>
2022-12-16 00:03:12 +0300

    day8-v

---
+++ assets/js/44-event-handling-practice.js
@@ -0,0 +1,18 @@
+const txtscore1El = document.querySelector("#txtscore1El");
+const txtscore2El = document.querySelector("#txtscore2El");
+const txtscore3El = document.querySelector("#txtscore3El");
+const btnAverage = document.querySelector("#btnAverage");
+
+btnAverage.addEventListener("click", () => {
+
+});
+
+const calculateAverage = (num1, num2, num3) => {
+    //ortalama islemi
+    return 15;
+} 
+
+const convertScoreToLetter = () => {
+    //Harfe cevirme islemi
+    return "A";
+}
