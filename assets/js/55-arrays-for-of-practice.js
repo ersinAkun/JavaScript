11995dc7d8696acf29f9ab0a02773473f48f8fc2
Ersin Akun <ersinakun34@gmail.com>
2022-12-17 23:07:11 +0300

    v4

---
+++ assets/js/55-arrays-for-of-practice.js
@@ -0,0 +1,16 @@
+import{countries} from "../data/countries.js";
+
+const loadData = () => {
+
+    let options = "";
+
+    for(let country of countries){
+        console.log(country);
+        options += `<option value="${country.ccn3}">${country.name.common}</option>`;
+    }
+
+    document.querySelector("#ddlCountries").innerHTML = options;
+
+
+}
+loadData();
