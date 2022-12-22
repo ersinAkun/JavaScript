777d6afa8ae8bc809c043b6999f47c10a091ab8d
Ersin Akun <ersinakun34@gmail.com>
2022-12-23 00:00:40 +0300

    day14-v2

--- assets/js/79-settimeout-practice.js
+++ assets/js/79-settimeout-practice.js
@@ -1,4 +1,16 @@
-
-document.querySelector("#menu").addEventListener("mouseenter", ()=>{
-    e.target.classList.add("opened");
+let menuTimeout = null;
+document.querySelector("#menu").addEventListener("mouseenter", (e)=>{
+    
+    menuTimeout = setTimeout( ()=>{
+        e.target.classList.add("opened");
+    },300 );
+    
+})
+document.querySelector("#menu").addEventListener("mouseleave", (e)=>{
+    e.target.classList.remove("opened");
+    if(menuTimeout !== null){
+        clearTimeout(menuTimeout);
+    }
+    
+    
 })
---
+++ assets/js/81-setinterval-practice.js
@@ -0,0 +1,37 @@
+
+
+const hourEl = document.querySelector("#clock li:nth-child(1)");
+const minuteEl = document.querySelector("#clock li:nth-child(3)");
+const secondEl = document.querySelector("#clock li:nth-child(2)");
+
+
+setInterval(()=>{   
+    const dateTime = new Date();
+    const hour = dateTime.getHours();
+    const minute = dateTime.getMinutes();
+
+    hourEl.innerHTML = hour;
+    minuteEl.innerHTML = minute;
+
+    secondEl.classList.toggle("hidden");
+
+
+    
+
+},1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
